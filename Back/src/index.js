5db759045fe4e76ddfeede43af6dc4cf746a8d8b
JUAN ESTEBAN CHAVEZ COLLAZOS <juanchavezzp@unicauca.edu.co>
2024-10-06 16:49:09 -0500

    Actualizacion CORS

--- Back/src/index.js
+++ Back/src/index.js
@@ -10,8 +10,15 @@ console.log('Servidor iniciado en ' + app.get("port"));
 
 // Middlewares
 app.use(cors({
-    origin: ["http://127.0.0.1:5500", "http://127.0.0.1:5501", "http://127.0.0.1:5502"]
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://127.0.0.1:5501",
+        "http://127.0.0.1:5502",
+        "http://localhost:5501",
+        "http://localhost:5500"  
+    ]
 }));
+
 
 app.use(express.json());
 
